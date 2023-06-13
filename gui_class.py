eef6191b97cf96018d3da299a34475145b3a19bf
BABYdotRAR <lopezoscar0101lp@gmail.com>
2023-06-12 23:32:13 -0600

    colors changed

--- gui_class.py
+++ gui_class.py
@@ -1,5 +1,5 @@
 import tkinter as tk
-import tkinter.font as tkFont
+from tkinter import font
 from tkinter import messagebox
 from qr_reader import get_data_from_qr
 
@@ -11,23 +11,72 @@ class GUI_UDI:
 
         self.config_root()
 
-        self.gothic_ui_14_font = tkFont.Font(family='Yu Gothic UI Light', size=14)
+        self.gothic_ui_light_14_font = font.Font(family='Yu Gothic UI Light', size=14)
+        self.gothic_ui_16_font = font.Font(family='Yu Gothic UI', size=16)
 
         self.welcome_label = tk.Label(
             self.root,
             text="Bienvenido de nuevo, por favor selecciona una opción",
-            font=self.gothic_ui_14_font
+            font=self.gothic_ui_16_font,
+            fg="white",
+            bg="#4C4A48"
         )
         self.welcome_label.pack(padx=10, pady=10)
 
         self.form_frame = tk.Frame(self.root)
+        self.config_form_frame()
 
+        self.init_string_vars()
+        self.init_dropdowns()
+
+        self.form_frame.pack(padx=10, pady=10, fill='x')
+
+        self.continue_btn = tk.Button(
+            self.root, 
+            text="Siguiente", 
+            font=self.gothic_ui_light_14_font,
+            bg="#038387",
+            fg="white",
+            command=self.on_continue
+        )
+        self.continue_btn.pack(side="right", padx=20, pady=20)
+
+        self.root.mainloop()
+
+
+    def config_root(self):
+        self.root.title("UDI ESIT")
+        self.root.geometry("800x600")
+        self.root.config(bg="#4C4A48")
+
+
+    def config_form_frame(self):
+        self.form_frame.config(bg="#4C4A48")
         self.form_frame.columnconfigure(0, weight=1)
         self.form_frame.columnconfigure(1, weight=1)
         self.form_frame.columnconfigure(2, weight=1)
 
-        self.init_string_vars()
 
+    def init_string_vars(self):
+        self.service_var = tk.StringVar()
+        self.service_var.set("Préstamo de computadora.")
+        self.service_var.trace("w", self.on_dropdown_changed)
+
+        self.classroom_control_var = tk.StringVar()
+        self.classroom_control_var.set("102")
+        self.projector_number_var = tk.StringVar()
+        self.projector_number_var.set("1")
+
+
+    def init_dropdowns(self):
+        self.service_label = tk.Label(
+            self.form_frame,
+            text="Seleccione el servicio solicitado:",
+            font=self.gothic_ui_light_14_font,
+            fg="white",
+            bg="#4C4A48"
+        )
+        self.service_label.grid(row=0, column=0, sticky=tk.W+tk.E)
         self.service_dropdown = tk.OptionMenu(
             self.form_frame, 
             self.service_var, 
@@ -35,13 +84,15 @@ class GUI_UDI:
             "Préstamo de cañón.", 
             "Préstamo de control."
         )
-        self.service_dropdown.config(font=self.gothic_ui_14_font)
+        self.service_dropdown.config(font=self.gothic_ui_light_14_font, bg="#0063B1", fg="white")
         self.service_dropdown.grid(row=1, column=0, sticky=tk.W+tk.E)
 
         self.control_label = tk.Label(
             self.form_frame,
             text="Seleccione el control a prestar:",
-            font=self.gothic_ui_14_font
+            font=self.gothic_ui_light_14_font,
+            fg="white",
+            bg="#4C4A48"
         )
         self.classroom_control_dropdown = tk.OptionMenu(
             self.form_frame, 
@@ -50,12 +101,14 @@ class GUI_UDI:
             "E-202", 
             "Lab 2"
         )
-        self.classroom_control_dropdown.config(font=self.gothic_ui_14_font)
+        self.classroom_control_dropdown.config(font=self.gothic_ui_light_14_font, bg="#0063B1", fg="white")
 
         self.projector_label = tk.Label(
             self.form_frame,
             text="Seleccione el número de cañón a prestar:",
-            font=self.gothic_ui_14_font
+            font=self.gothic_ui_light_14_font,
+            fg="white",
+            bg="#4C4A48"
         )
         self.projector_number_dropdown = tk.OptionMenu(
             self.form_frame, 
@@ -64,32 +117,7 @@ class GUI_UDI:
             "2", 
             "3"
         )
-        self.projector_number_dropdown.config(font=self.gothic_ui_14_font)
-
-        self.form_frame.pack(padx=10, pady=10)
-
-        self.continue_btn = tk.Button(
-            self.root, 
-            text="Siguiente", 
-            font=self.gothic_ui_14_font,
-            command=self.on_continue
-        )
-        self.continue_btn.pack(padx=20, pady=20)
-
-        self.root.mainloop()
-
-
-    def config_root(self):
-        self.root.title("UDI ESIT")
-        self.root.geometry("800x600")
-
-
-    def init_string_vars(self):
-        self.service_var = tk.StringVar()
-        self.service_var.trace("w", self.on_dropdown_changed)
-
-        self.classroom_control_var = tk.StringVar()
-        self.projector_number_var = tk.StringVar()
+        self.projector_number_dropdown.config(font=self.gothic_ui_light_14_font, bg="#0063B1", fg="white")
 
 
     def on_continue(self):
@@ -107,14 +135,14 @@ class GUI_UDI:
         option = self.service_var.get()
 
         if option == "Préstamo de cañón.":
-            self.projector_label.grid(row=0, column=1, sticky=tk.W+tk.E)
-            self.projector_number_dropdown.grid(row=0, column=2, sticky=tk.W+tk.E)
+            self.projector_label.grid(row=2, column=1, sticky=tk.W+tk.E)
+            self.projector_number_dropdown.grid(row=2, column=2, sticky=tk.W+tk.E)
 
             self.control_label.grid_forget()
             self.classroom_control_dropdown.grid_forget()
         elif option == "Préstamo de control.":
-            self.control_label.grid(row=0, column=1, sticky=tk.W+tk.E)
-            self.classroom_control_dropdown.grid(row=0, column=2, sticky=tk.W+tk.E)
+            self.control_label.grid(row=2, column=1, sticky=tk.W+tk.E)
+            self.classroom_control_dropdown.grid(row=2, column=2, sticky=tk.W+tk.E)
 
             self.projector_label.grid_forget()
             self.projector_number_dropdown.grid_forget()
